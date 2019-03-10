410c5ee724b69ad998132b7b867f95874418b9a3
mervin serviano <mervinserviano@yahoo.com>
2019-03-10 13:28:25 -0700

    populated the folders

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Bob Smith",
+    photo: "http://mediad.publicbroadcasting.net/p/wxxi/files/styles/x_large/public/201703/bob_smith_photo2-new.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Michael Doe",
+    photo: "http://s3.amazonaws.com/csvweb/obituaries/photos/5535/575338/5c27b90ab99ee.JPG",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Larry Johnson",
+    photo: "https://pmcdeadline2.files.wordpress.com/2017/03/taylor-john-smith-2.jpg?crop=249px%2C25px%2C873px%2C586px&resize=446%2C299",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Jose Sanchez",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSebWbtYFnG2PomCjQ8wVX-qBOjTdQ01h5hm2AzLYaQ7HWbf2fWrw",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Mike Jorden",
+    photo: "https://pbs.twimg.com/profile_images/2221073268/John_Headshot_-_square.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
